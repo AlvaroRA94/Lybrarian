8eb697837c9dbb7adecfe4f315225ea79597989e
Álvaro Roldán <roldan.antequera@gmail.com>
2024-02-16 11:50:21 +0100

    first commit

---
+++ src/logica/Biblioteca.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import igu.MainPage;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Biblioteca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainPage inicio = new MainPage();
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+        
+    }
+    
+}
